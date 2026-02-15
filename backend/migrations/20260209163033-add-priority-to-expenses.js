ac6cfcd8dafcfd850840b2dee0af41e05fb6375e
bikash-yadav20 <nishantyadav2488@gmail.com>
2026-02-15 17:49:14 +0530

    there was spelling mistake in migration file and i corrected it

--- backend/migrations/20260209163033-add-priority-to-expenses.js
+++ backend/migrations/20260209163033-add-priority-to-expenses.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Expenses", "priority", {
+    await queryInterface.addColumn("expenses", "priority", {
       type: Sequelize.BOOLEAN,
       defaultValue: false,
       allowNull: false,
@@ -11,6 +11,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Expenses", "priority");
+    await queryInterface.removeColumn("expenses", "priority");
   },
 };
