e69228d2ef55d64415685c00353e41bbf5eb8dd1
bikash-yadav20 <nishantyadav2488@gmail.com>
2026-03-21 22:42:44 +0530

    improved security of cash controller and xlsxExport

--- backend/src/routes/cashController.js
+++ backend/src/routes/cashController.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const cashController = require("../controllers/cashController");
+const auth = require("../middleware/auth");
 
 // Opening balance (start day)
-router.post("/opening", cashController.startDay);
+router.post("/opening", auth, cashController.startDay);
 
 // Closing balance (close day)
-router.post("/closing", cashController.closeDay);
+router.post("/closing", auth, cashController.closeDay);
 
 //get Closing balance from database
 
-router.get("/opening-balance", cashController.getClosingBalance);
+router.get("/opening-balance", auth, cashController.getClosingBalance);
 
 module.exports = router;
--- backend/src/routes/xlsxExport.js
+++ backend/src/routes/xlsxExport.js
@@ -1,7 +1,8 @@
 const { exportDailyLedger } = require("../controllers/xlsxExport");
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/daily-ledger", exportDailyLedger);
+router.get("/daily-ledger", auth, exportDailyLedger);
 
 module.exports = router;
